263e8efa82cf523e6982fc3c07e5d3fa69ee7bfb
FabioRosado <fabioglrosado@gmail.com>
2017-09-28 13:37:21 +0100

    Added two new functions

--- __init__.py
+++ __init__.py
@@ -1,17 +1,23 @@
 from opsdroid.matchers import match_regex
 
 import requests
+import logging
 
 
-base_url = "http://api.openweathermap.org/data/2.5/"
+_LOGGER = logging.getLogger("opsdroid")
 
 
-@match_regex(r'weather', case_sensitive=False)
-async def weather(opsdroid, config, message):
+def get_weather(config):
+    base_url = "http://api.openweathermap.org/data/2.5/"
     api_url = "weather?q={}&units={}&appid={}".format(
-               config['city'], config['unit'], config['api-key'])
-    weather = requests.get(base_url+api_url).json()
+        config['city'], config['unit'], config['api-key'])
+    weather_data = requests.get(base_url + api_url).json()
+    return weather_data
 
+
+@match_regex(r'(?:how|what)(?:\'s|s| is) the weather', case_sensitive=False)
+async def weather(opsdroid, config, message):
+    weather = get_weather(config)
     temp = weather['main']['temp']
     humidity = weather['main']['humidity']
     city = weather['name']
@@ -20,3 +26,22 @@ async def weather(opsdroid, config, message):
     await message.respond("It's currently {} degrees, {}% humidity "
                           "in {} and {} is forecasted for today".format(temp, humidity, city, status))
 
+
+@match_regex(r'is it (?:cold|hot|warm|nice outside)', case_sensitive=False)
+async def cold_outside(opsdroid, config, message):
+    weather = get_weather(config)
+    temp = weather['main']['temp']
+
+    if temp < 10 or temp < 50 and config['unit'] == 'imperial':
+        await message.respond("It's pretty cold today! It's currently "
+                              "{} degrees outside".format(temp))
+    elif temp > 10 < 19 or temp > 50 < 65:
+        await message.respond("I think it's better if you take a "
+                              "jacket with you today. It's currently "
+                              "{} degrees outside".format(temp))
+    elif temp > 19 < 23 or temp > 65 < 75:
+        await message.respond("It's not too bad actually, it's currently "
+                              "{} degrees outside".format(temp))
+    elif temp > 23 or temp > 75:
+        await message.respond("It's getting pretty hot, it's currently "
+                              "{} degrees outside".format(temp))
