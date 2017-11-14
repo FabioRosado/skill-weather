0ad2d40ac522859bfa911461f0eade581632e1d2
FabioRosado <fabioglrosado@gmail.com>
2017-11-14 12:13:21 +0000

    Replace requests with aiohttp

--- __init__.py
+++ __init__.py
@@ -1,32 +1,35 @@
 from opsdroid.matchers import match_regex
 
-import requests
+import aiohttp
 
 
-def get_weather(config):
+async def get_weather(config):
     base_url = "http://api.openweathermap.org/data/2.5/"
     api_url = "weather?q={}&units={}&appid={}".format(
         config['city'], config['unit'], config['api-key'])
-    weather_data = requests.get(base_url + api_url).json()
 
-    return weather_data
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(base_url + api_url)
+
+    return await response.json()
 
 
 @match_regex(r'(?:how|what)(?:\'s|s| is) the weather', case_sensitive=False)
 async def tell_weather(opsdroid, config, message):
-    weather = get_weather(config)
+
+    weather = await get_weather(config)
     temp = weather['main']['temp']
     humidity = weather['main']['humidity']
     city = weather['name']
     status = weather['weather'][0]['main']
-
+    
     await message.respond("It's currently {} degrees, {}% humidity in {} and {} is forecasted "
                           "for today".format(temp, humidity, city, status))
 
 
 @match_regex(r'is it (?:cold|hot|warm|nice outside)', case_sensitive=False)
 async def cold_outside(opsdroid, config, message):
-    weather = get_weather(config)
+    weather = await get_weather(config)
     temp = weather['main']['temp']
 
     if temp < 10 or config['unit'] == 'imperial' and temp < 50:
